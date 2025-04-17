096616209e1782ad6078991f603776cdb3f3092e
Tung-Yen Wang <annawang9909@gmail.com>
2025-04-17 14:07:05 -0400

    minor modifications

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        <title>HUEY - Get in Line</title>
+        <meta name="description" content="Join the waitlist at your favorite restaurants from your phone" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
